adbddf09b5e28a6a148b488d65447c8f61b020e8
ninana123 <68698439+ninana123@users.noreply.github.com>
2021-01-23 23:22:07 +0300

    Update script.js

--- q/js/script.js
+++ q/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$('.slides__wrapper').slick({
 		speed: 1200,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrow/arrow_left.svg"</button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow/arrow_left.svg"</button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow/arrow_right.svg"</button>',
 		responsive: [
 			{
